8779d25ae0e1fbfd1cf6340758ae59d4f7b5376d
seul-dev <seul.dev@gmail.com>
2022-07-26 21:17:20 +0900

    webpack 빌드

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: '18.2.0',
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    'react/prop-types': 0,
+  },
+};
---
+++ webpack.config.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+const path = require('path');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'docs'),
+    },
+    port: 3001,
+    hot: true,
+  },
+  plugins: [new ReactRefreshPlugin()],
+});
